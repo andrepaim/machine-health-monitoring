09279f2c238c94e2173cf72bc18ec142773b3aed
Andre  Lemos <andrepaim@gmail.com>
2023-11-14 09:22:35 -0300

    Update data_processor.cpp

--- data_processor.cpp
+++ data_processor.cpp
@@ -4,33 +4,16 @@
 #include <thread>
 #include <unistd.h>
 #include <bson.h>
-#include <mongoc.h>
 #include "json.hpp" 
 #include "mqtt/client.h" 
 
 #define QOS 1
 #define BROKER_ADDRESS "tcp://localhost:1883"
+#define GRAPHITE_HOST "graphite"
+#define GRAPHITE_PORT 2003
 
-void insert_document(mongoc_collection_t *collection, std::string machine_id, std::string timestamp_str, int value) {
-    bson_error_t error;
-    bson_oid_t oid;
-    bson_t *doc;
-    
-    std::tm tm{};
-    std::istringstream ss{timestamp_str};
-    ss >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%SZ");
-    std::time_t time_t_timestamp = std::mktime(&tm);
+void insert_document(const std::string& machine_id,const& std::string sensor_id,const& std::string timestamp_str,const  int value) {
 
-    doc = bson_new();
-    BSON_APPEND_UTF8(doc, "machine_id", machine_id.c_str());
-    BSON_APPEND_TIME_T(doc, "timestamp", time_t_timestamp);
-    BSON_APPEND_INT32(doc, "value", value);
-
-    if (!mongoc_collection_insert_one(collection, doc, NULL, NULL, &error)) {
-        std::cerr << "Failed to insert doc: " << error.message << std::endl;
-    }
-
-    bson_destroy(doc);
 }
 
 std::vector<std::string> split(const std::string &str, char delim) {
@@ -45,12 +28,7 @@ std::vector<std::string> split(const std::string &str, char delim) {
 
 int main(int argc, char* argv[]) {
     std::string clientId = "clientId";
-    mqtt::client client(BROKER_ADDRESS, clientId);
 
-    // Initialize MongoDB client and connect to the database.
-    mongoc_init();
-    mongoc_client_t *mongodb_client = mongoc_client_new("mongodb://localhost:27017");
-    mongoc_database_t *database = mongoc_client_get_database(mongodb_client, "sensors_data"); // replace with your database name
 
     // Create an MQTT callback.
     class callback : public virtual mqtt::callback {
@@ -70,10 +48,7 @@ int main(int argc, char* argv[]) {
             std::string timestamp = j["timestamp"];
             int value = j["value"];
 
-            // Get collection and persist the document.
-            mongoc_collection_t *collection = mongoc_database_get_collection(db, sensor_id.c_str());
-            insert_document(collection, machine_id, timestamp, value);
-            mongoc_collection_destroy(collection);
+            insert_document(machine_id, sensor_id, timestamp, value);
         }
     };
 
@@ -96,11 +71,6 @@ int main(int argc, char* argv[]) {
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
-
-    // Cleanup MongoDB resources
-    mongoc_database_destroy(database);
-    mongoc_client_destroy(mongodb_client);
-    mongoc_cleanup();
 
     return EXIT_SUCCESS;
 }
