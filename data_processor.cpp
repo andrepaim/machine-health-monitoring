4947ccf611a03f64425fff9744f07832ca441775
Andre  Lemos <andrepaim@gmail.com>
2023-11-14 12:37:19 +0000

    Replace MongoDB with Graphite

--- data_processor.cpp
+++ data_processor.cpp
@@ -3,34 +3,14 @@
 #include <chrono>
 #include <thread>
 #include <unistd.h>
-#include <bson.h>
-#include <mongoc.h>
 #include "json.hpp" 
 #include "mqtt/client.h" 
 
 #define QOS 1
 #define BROKER_ADDRESS "tcp://localhost:1883"
 
-void insert_document(mongoc_collection_t *collection, std::string machine_id, std::string timestamp_str, int value) {
-    bson_error_t error;
-    bson_oid_t oid;
-    bson_t *doc;
-    
-    std::tm tm{};
-    std::istringstream ss{timestamp_str};
-    ss >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%SZ");
-    std::time_t time_t_timestamp = std::mktime(&tm);
+void post_metric(const std::string& machine_id, const std::string& sensor_id, const std::string& timestamp_str, const int value) {
 
-    doc = bson_new();
-    BSON_APPEND_UTF8(doc, "machine_id", machine_id.c_str());
-    BSON_APPEND_TIME_T(doc, "timestamp", time_t_timestamp);
-    BSON_APPEND_INT32(doc, "value", value);
-
-    if (!mongoc_collection_insert_one(collection, doc, NULL, NULL, &error)) {
-        std::cerr << "Failed to insert doc: " << error.message << std::endl;
-    }
-
-    bson_destroy(doc);
 }
 
 std::vector<std::string> split(const std::string &str, char delim) {
@@ -47,17 +27,9 @@ int main(int argc, char* argv[]) {
     std::string clientId = "clientId";
     mqtt::client client(BROKER_ADDRESS, clientId);
 
-    // Initialize MongoDB client and connect to the database.
-    mongoc_init();
-    mongoc_client_t *mongodb_client = mongoc_client_new("mongodb://localhost:27017");
-    mongoc_database_t *database = mongoc_client_get_database(mongodb_client, "sensors_data"); // replace with your database name
-
     // Create an MQTT callback.
     class callback : public virtual mqtt::callback {
-        mongoc_database_t *db;
-
     public:
-        callback(mongoc_database_t *db) : db(db) {}
 
         void message_arrived(mqtt::const_message_ptr msg) override {
             auto j = nlohmann::json::parse(msg->get_payload());
@@ -70,14 +42,11 @@ int main(int argc, char* argv[]) {
             std::string timestamp = j["timestamp"];
             int value = j["value"];
 
-            // Get collection and persist the document.
-            mongoc_collection_t *collection = mongoc_database_get_collection(db, sensor_id.c_str());
-            insert_document(collection, machine_id, timestamp, value);
-            mongoc_collection_destroy(collection);
+            post_metric(machine_id, sensor_id, timestamp, value);
         }
     };
 
-    callback cb(database);
+    callback cb;
     client.set_callback(cb);
 
     // Connect to the MQTT broker.
@@ -96,11 +65,6 @@ int main(int argc, char* argv[]) {
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
-
-    // Cleanup MongoDB resources
-    mongoc_database_destroy(database);
-    mongoc_client_destroy(mongodb_client);
-    mongoc_cleanup();
 
     return EXIT_SUCCESS;
 }
